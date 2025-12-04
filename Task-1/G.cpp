e7fef08d9571171d70be533f8963a8afce8203b3
Mohamed Nael <mohamed.nael2005@gmail.com>
2025-12-04 20:23:17 +0200

    Add files via upload

---
+++ Task-1/G.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int k, r;
+    cin >> k >> r;
+    for (int n = 1; ; n++) {
+        int total = n * k;
+        if (total % 10 == 0 || total % 10 == r) {
+            cout << n;
+            break;
+        }
+    }
+}
